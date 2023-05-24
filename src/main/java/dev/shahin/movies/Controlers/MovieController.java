b5c3946b3430297de80dad318c46b661f1d1826f
shahinyusifli <sahinyusifli98@gmail.com>
2023-05-24 17:26:15 +0300

    Create endpoint for get single movie

--- src/main/java/dev/shahin/movies/Controlers/MovieController.java
+++ src/main/java/dev/shahin/movies/Controlers/MovieController.java
@@ -4,6 +4,7 @@ package dev.shahin.movies.Controlers;
 import dev.shahin.movies.Entities.Movie;
 import dev.shahin.movies.Repositories.MovieRepository;
 import dev.shahin.movies.Services.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -23,5 +25,10 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getMovies() {
         return new ResponseEntity<List<Movie>>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id) {
+        return new ResponseEntity<Optional<Movie>>(movieService.getSingleMovie(id), HttpStatus.OK);
     }
 }
--- src/main/java/dev/shahin/movies/Services/MovieService.java
+++ src/main/java/dev/shahin/movies/Services/MovieService.java
@@ -2,10 +2,12 @@ package dev.shahin.movies.Services;
 
 import dev.shahin.movies.Entities.Movie;
 import dev.shahin.movies.Repositories.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -16,4 +18,6 @@ public class MovieService {
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
     }
+
+    public Optional<Movie> getSingleMovie(ObjectId id) { return movieRepository.findById(id); }
 }
