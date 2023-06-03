0088174b1800abe3ebb9e4d9ccbbc9f7bf5a4d09
shahinyusifli <sahinyusifli98@gmail.com>
2023-06-03 18:05:43 +0300

    Modify excpetion handler

--- src/main/java/dev/shahin/movies/Utilities/Exception/ApplicationExceptionResponse.java
+++ src/main/java/dev/shahin/movies/Utilities/Exception/ApplicationExceptionResponse.java
@@ -3,14 +3,17 @@ package dev.shahin.movies.Utilities.Exception;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 import java.time.LocalDateTime;
 
-@Builder
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ApplicationExceptionResponse {
     private String message;
-    private int status;
+    private String status;
     private LocalDateTime time;
 }
--- src/main/java/dev/shahin/movies/Utilities/Exception/GlobalExceptionHandler.java
+++ src/main/java/dev/shahin/movies/Utilities/Exception/GlobalExceptionHandler.java
@@ -1,18 +1,19 @@
+package dev.shahin.movies.Utilities.Exception;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
 
-public class ValidationHandler {
-
-    public static ResponseEntity<Map<String, String>> handleValidationErrors(BindingResult bindingResult) {
-        Map<String, String> errors = new HashMap<>();
-        for (FieldError error : bindingResult.getFieldErrors()) {
-            errors.put(error.getField(), error.getDefaultMessage());
-        }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
-    }
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        ApplicationExceptionResponse exceptionResponse = new ApplicationExceptionResponse(ex.getMessage(), ex.getStatusCode().toString(), LocalDateTime.now());
+        return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NO_CONTENT);
+    };
 }
