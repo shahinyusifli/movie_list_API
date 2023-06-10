60919ff20267ca293e817e96d64631a205abae72
shahinyusifli <sahinyusifli98@gmail.com>
2023-06-10 15:56:21 +0300

    Modified DTO for creating movie

--- src/main/java/dev/shahin/movies/Entities/MovieCreateDTO.java
+++ src/main/java/dev/shahin/movies/Entities/MovieCreateDTO.java
@@ -1,2 +1,21 @@
-package dev.shahin.movies.Entities;public class MovieCreateDTO {
+package dev.shahin.movies.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("movies")
+public class MovieCreateDTO {
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private  String poster;
+    private List<String> genres;
 }
