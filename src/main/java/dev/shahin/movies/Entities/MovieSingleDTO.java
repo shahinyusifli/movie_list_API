7b40f81c48f73535c5af24a6aae75f58ed32f881
shahinyusifli <sahinyusifli98@gmail.com>
2023-06-10 10:54:54 +0300

    Entity class for single movie

--- src/main/java/dev/shahin/movies/Entities/MovieSingleDTO.java
+++ src/main/java/dev/shahin/movies/Entities/MovieSingleDTO.java
@@ -1,2 +1,17 @@
-package dev.shahin.movies.Entities;public class MovieSingleDTO {
+package dev.shahin.movies.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieSingleDTO {
+    private String title;
+    private String releaseDate;
+    private List<ObjectId> reviewIds;
 }
