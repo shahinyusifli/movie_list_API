4cebc66846e4d97974438a526ea8483f8a89e70c
Shahin Yusifli <sahinyusifli98@gmail.com>
2023-09-29 01:23:04 +0300

    Add tests for services

--- src/main/java/dev/shahin/movies/entity/MovieWithReviewsDTO.java
+++ src/main/java/dev/shahin/movies/entity/MovieWithReviewsDTO.java
@@ -1,10 +1,12 @@
 package dev.shahin.movies.entity;
 
+import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Collection;
 import java.util.List;
 
 @Data
--- src/test/java/dev/shahin/movies/service/MovieServiceTest.java
+++ src/test/java/dev/shahin/movies/service/MovieServiceTest.java
@@ -1,7 +1,11 @@
 package dev.shahin.movies.service;
 
+import com.mongodb.client.result.UpdateResult;
 import dev.shahin.movies.entity.Movie;
+import dev.shahin.movies.entity.MovieGetDTO;
 import dev.shahin.movies.entity.MovieSingleDTO;
+
+import org.bson.BsonValue;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,11 +13,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.bson.assertions.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.times;
@@ -46,4 +54,57 @@ public class MovieServiceTest {
 
         verify(mongoTemplate, times(1)).find(any(Query.class), eq(Movie.class));
     }
+
+    @Test
+    public void testGetAllMovies() {
+
+        List<MovieGetDTO> movieGetDTOList = new ArrayList<>();
+
+        Query expectedQuery = new Query();
+        expectedQuery.addCriteria(Criteria.where("flag").is(false));
+        expectedQuery.fields()
+                .include("title")
+                .include("releaseDate")
+                .include("genres")
+                .include("poster");
+
+        when(mongoTemplate.find(eq(expectedQuery), eq(MovieGetDTO.class))).thenReturn(movieGetDTOList);
+
+        List<MovieGetDTO> result = movieService.getAllMovies();
+
+        verify(mongoTemplate, times(1)).find(eq(expectedQuery), eq(MovieGetDTO.class));
+    }
+
+    @Test
+    public void testDeleteMovie() {
+        ObjectId objectId = new ObjectId();
+        Movie movie = new Movie();
+
+        when(mongoTemplate.updateFirst(any(Query.class), any(Update.class), eq(Movie.class)))
+                .thenReturn(new UpdateResult() {
+                    @Override
+                    public boolean wasAcknowledged() {
+                        return false;
+                    }
+
+                    @Override
+                    public long getMatchedCount() {
+                        return 0;
+                    }
+
+                    @Override
+                    public long getModifiedCount() {
+                        return 0;
+                    }
+
+                    @Override
+                    public BsonValue getUpsertedId() {
+                        return null;
+                    }
+                });
+        Long result = movieService.deleteMovie(objectId);
+        verify(mongoTemplate, times(1)).updateFirst(any(Query.class), any(Update.class), eq(Movie.class));
+        assertNotNull(result);
+    }
+
 }
